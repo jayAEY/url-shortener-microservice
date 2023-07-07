043823b1c709ac6386b6f97c71836f2b1519e4e0
jayAEY <106685702+jayAEY@users.noreply.github.com>
2023-07-07 09:20:51 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const bodyParser = require('body-parser')
+const isUrl = require('is-url')
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended:false }))
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -21,4 +24,25 @@ app.get('/api/hello', function(req, res) {
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
+});
+
+let newUrl = 0
+let newUrls = {}
+
+app.post('/api/shorturl', function(req, res) {
+  newUrl += 1
+  let inputUrl = req.body.url
+  if(!isUrl(inputUrl)) {
+   res.json({ error: 'invalid url' }) 
+  }
+  newUrls[newUrl] = inputUrl
+   res.json({ original_url : inputUrl, short_url : newUrl});
+});
+
+app.get('/api/shorturl/:new', function(req, res) {
+  let newUrl = req.params.new
+   if(newUrls[newUrl] == undefined){ 
+     res.json({ error: 'undefined' }) 
+   }
+   res.redirect(newUrls[newUrl]);
 });
